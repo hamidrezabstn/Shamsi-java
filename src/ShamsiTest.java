18616a2683387629ba70bd77a3ebe169c07f0eba
Hamidreza Bistooni <hamidrezabstn@gmail.com>
2015-02-21 17:02:51 +0330

    Initiling Shamsi Project

---
+++ src/ShamsiTest.java
@@ -0,0 +1,12 @@
+
+public class ShamsiTest {
+
+	public static void main(String[] args) {
+		
+		
+		ToPersian test = new ToPersian();
+		
+		System.out.println(test.getCurrentShamsidate());
+	}
+
+}
